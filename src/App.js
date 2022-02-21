ae4e0f65c82b5ed1d21f39193832ccb0c0fb914f
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-02-21 20:27:44 +0530

    feat: do initial code-base clean up and make an route structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="app" component={DashBoardLayout}>
+          <Route path="home" component={Home} />
+          <Route path="add" component={Add} />
+        </Route>
+        {/* <Route path="user" component={UserLayout}>
+          <Route path="login" component={Login} />
+          <Route path="register" component={Register} />
+        </Route> */}
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/layouts/DashBoardLayout.js
@@ -0,0 +1,6 @@
+import { styled } from "@mui/material";
+export default function DashBoardLayout(){
+    return(
+        
+    )
+}
