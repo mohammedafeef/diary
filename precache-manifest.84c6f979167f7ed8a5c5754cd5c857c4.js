1f339d4f9b430eb036de66aa8dbf10b0584c26f2
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-03-01 11:24:32 +0530

    Updates

---
+++ precache-manifest.84c6f979167f7ed8a5c5754cd5c857c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfa7c0a915c0f42c18d4ba287f3f825",
+    "url": "/diary/index.html"
+  },
+  {
+    "revision": "4c02f758c3e0b48c7030",
+    "url": "/diary/static/css/main.6cf8aa57.chunk.css"
+  },
+  {
+    "revision": "f79b4bc94e86f89303b4",
+    "url": "/diary/static/js/2.5316bf31.chunk.js"
+  },
+  {
+    "revision": "4c02f758c3e0b48c7030",
+    "url": "/diary/static/js/main.c2531ee8.chunk.js"
+  },
+  {
+    "revision": "96d2ce29ffc9c1de843c",
+    "url": "/diary/static/js/runtime~main.99875d05.js"
+  }
+]);
